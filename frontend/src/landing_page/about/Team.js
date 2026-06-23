7310d92a691985675c5d46918dddbade6cd764db
Umangi-webdev <umppgecpl2005@gmail.com>
2026-06-23 19:00:42 +0530

    Fixed image path from interface

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -13,7 +13,7 @@ function Team() {
       >
         <div className="col-6 p-3 text-center">
           <img
-            src="/media/images/nithinKamath.jpeg" alt="Team Member"
+            src="media/images/nithinKamath.jpeg" alt="CEO"
             style={{ borderRadius: "100%", width: "50%" }}
           />
           <h4 className="mt-5">Nithin Kamath</h4>
